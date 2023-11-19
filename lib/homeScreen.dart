1ee9a3e5ec70f4891d7e16a12e85d3184eaaf942
Abdallh-Oudaa <145578823+Abdallh-Oudaa@users.noreply.github.com>
2023-11-19 22:03:43 +0200

    splash screen with light mode and dark mode by using flutter_native_splash,

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,17 +1,8 @@
-import 'package:apptest/classData.dart';
-import 'package:apptest/itemWidget.dart';
 import 'package:flutter/material.dart';
 
 class home_screen extends StatelessWidget {
-  List<category> categoryList = [];
-  home_screen() {
-    data();
-  }
-  void data() {
-    for (int i = 0; i < 100; i++) {
-      categoryList.add(category(title: "title ${i}", path: "assets/code.jpg"));
-    }
-  }
+  static String routName = "homeScreen";
+  home_screen();
 
   @override
   Widget build(BuildContext context) {
@@ -24,19 +15,6 @@ class home_screen extends StatelessWidget {
             style: TextStyle(color: Colors.white),
           )),
           backgroundColor: Colors.blue,
-        ),
-        body: Container(
-          padding: EdgeInsets.all(4),
-          child: GridView.builder(
-              gridDelegate:  const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 2,
-                  mainAxisSpacing: 2
-                  ),
-              itemBuilder: (context, index) => item_widget(
-                    title: categoryList[index].title,
-                    path:categoryList[index].path,
-                  ),itemCount: categoryList.length,),
         ),
       ),
     );
